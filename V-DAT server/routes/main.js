2b280664c2ceba8d3ed0239b3546d7f7058bb52d
ssammoo <hjth0911@ajou.ac.kr>
2021-08-28 18:38:22 +0900

    Delete not use code

--- V-DAT server/routes/main.js
+++ V-DAT server/routes/main.js
@@ -6,14 +6,7 @@ const dbPromise = require('./database/databasePromise');
 
 
 router.post('/getData', (req, res) => {
-    let video_id=1
-    let sql = 'select * from video where id = ?;' +
-        'select timelines.id, time,timer,data,label from video left join timelines on video.id = timelines.video_id where video.id=? order by timelines.id'
-    let param = [video_id,video_id]
-    db.pool.query(sql, param, (err, rows) => {
-        console.log(err)
-        res.json(rows)
-    })
+    
 
 })
 
