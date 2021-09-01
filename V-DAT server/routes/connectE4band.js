c6ea5cad6c029597f8d223d8176eba8ef2636a69
ssammoo <hjth0911@ajou.ac.kr>
2021-09-01 17:48:40 +0900

    Change E4 connecting code

--- V-DAT server/routes/connectE4band.js
+++ V-DAT server/routes/connectE4band.js
@@ -13,70 +13,57 @@ var date = moment().format('YYYY-MM-DD HH:mm:ss');
 
 var serverip = '210.107.197.101';
 const e4port = 28000;
-var oldDevice = '';
 
 
-const mysql = require('mysql');
 
-router.get('/getName', function (req, res){
-    const sql = "select name, thumbnail from vr;"
-    db.pool.query(sql, function (err, result){
-        if(err){
-            console.log(err)
-        }else{
-            res.json(result)
-        }
-    })
-})
+router.post('/start', function (req, res){
+    let connect = req.body.state;
+    let deviceId = req.body.deviceId;
+    let serverIp = req.body.serverIp;
+    let e4Port = req.body.e4Port;
+    let sensors = req.body.sensor;
 
 
-router .post('/start', function (req, res){
-    var connect = req.body.state;
-    var deviceId = "F0CB11";
-    var user = req.body.user;
-    var logId = null;
-    const sql = "insert into E4_band (device_name) values (?);"
+    const sql = "insert into E4_band (device_name) values (?);";
 
+    sList = [EmpaticaE4.E4_ACC, EmpaticaE4.E4_BVP, EmpaticaE4.E4_TEMP, EmpaticaE4.E4_GSR, EmpaticaE4.E4_IBI]
 
-    // for(let i =0; i<oldDevice.length; i++){
-    //     if (deviceId == oldDevice[i].device_name ){
-    //         break;
-    //     }else{
-    //         db.pool.query(sql, [deviceId], function (err, r){
-    //             if(err) {
-    //                 console.log(err)
-    //             }
-    //         })
-    //     }
-    // }
-
+    let selectS = null;
+    
     if(connect == true){
-        dev1.connect(e4port, serverip, deviceId, function (data) {
+        dev1.connect(e4Port, serverIp, deviceId, function (data) {
             var sensorData = EmpaticaE4.getString(data);
             let sql = "INSERT INTO E4Low (value) VALUES (?);";
-
-
-
+            
+            
+            
             db.pool.query(sql,[sensorData],function (err,result){
                 if(err){
                     console.log(err);
                 }
             })
-
+            
         })
 
-        setTimeout(function () {
-            dev1.subscribe(EmpaticaE4.E4_ACC);
-            dev1.subscribe(EmpaticaE4.E4_BVP);
-        }, 1500);
-        setTimeout(function () {
-            dev1.subscribe(EmpaticaE4.E4_TEMP);
-            dev1.subscribe(EmpaticaE4.E4_GSR);
-        }, 2000)
+        sensors.forEach(element => {
+            selectS = sList[element];
+            setTimeout(function () {
+                dev1.subscribe(selectS);
+            }, 500);
+        });
 
-        setTimeout(function () {
-            dev1.subscribe(EmpaticaE4.E4_IBI);
-        }, 3500)
+        // setTimeout(function () {
+        //     dev1.subscribe(EmpaticaE4.E4_ACC);
+        //     dev1.subscribe(EmpaticaE4.E4_BVP);
+        // }, 1500);
+        // setTimeout(function () {
+        //     dev1.subscribe(EmpaticaE4.E4_TEMP);
+        //     dev1.subscribe(EmpaticaE4.E4_GSR);
+        // }, 2000)
+
+        // setTimeout(function () {
+        //     dev1.subscribe(EmpaticaE4.E4_IBI);
+        // }, 3500)
     }
     res.send("start Ok")
 })
@@ -84,22 +71,22 @@ router .post('/start', function (req, res){
 router.post('/end', function (req, res){
     var connect = req.body.state;
 
-    console.log(connect)
+    dev1.disconnect()     
 
-    if(connect == false){
-        setTimeout(function () {
-            dev1.unsubscribe(EmpaticaE4.E4_ACC);
-            dev1.unsubscribe(EmpaticaE4.E4_BVP);
-        }, 1500);
-        setTimeout(function () {
-            dev1.unsubscribe(EmpaticaE4.E4_TEMP);
-            dev1.unsubscribe(EmpaticaE4.E4_GSR);
-        }, 2000)
+    // if(connect == false){
+    //     setTimeout(function () {
+    //         dev1.unsubscribe(EmpaticaE4.E4_ACC);
+    //         dev1.unsubscribe(EmpaticaE4.E4_BVP);
+    //     }, 1500);
+    //     setTimeout(function () {
+    //         dev1.unsubscribe(EmpaticaE4.E4_TEMP);
+    //         dev1.unsubscribe(EmpaticaE4.E4_GSR);
+    //     }, 2000)
 
-        setTimeout(function () {
-            dev1.unsubscribe(EmpaticaE4.E4_IBI);
-        }, 3500)
-    }
+    //     setTimeout(function () {
+    //         dev1.unsubscribe(EmpaticaE4.E4_IBI);
+    //     }, 3500)
+    // }
 
     res.send('End Ok');
 })
