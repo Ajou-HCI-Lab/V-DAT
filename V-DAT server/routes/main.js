20017eece8b8b14b8bd108add5a00648915992d0
ssammoo <hjth0911@ajou.ac.kr>
2021-09-14 00:33:28 +0900

    Add connecting DB, download csv file, request RESTFUL API, Promise code

--- V-DAT server/routes/main.js
+++ V-DAT server/routes/main.js
@@ -1,26 +1,77 @@
 var express = require('express');
 var router = express.Router();
 const db = require('./database/database');
+const axios = require('axios');
+let fs = require('fs');
 const dbPromise = require('./database/databasePromise');
 /* GET users listing. */
 
+const getAnal = () => {
+    try {
+        return axios.get('http://192.168.35.9:80/hello');
+    } catch (error) {
+        console.log(error)
+    }
+
+};
+
+const getReturn = () => {
+    const r = getAnal()
+        .then(response => {
+            console.log(response.data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+};
 
 router.post('/getData', (req, res) => {
-    
-    const sql = "select * from e4Acc " +
-                "INTO OUTFILE '~/e4Acc.csv' " +
-                "FIELDS TERMINATED BY ',' " + 
-                "LINES TERMINATED BY '\n';";
 
-    db.pool.query(sql, function(err, res){
-        try{
-            console.log(err);
-        }catch{
-            console.log(res);
-        }
-    })
+    const sql = "select * from E4Low;";
+    const date = new Date();
 
-    res.send('Done');
+
+    getReturn();
+
+
+    // db.pool.query(sql, function(err, result){
+    //     if(err){
+    //         console.log(err);
+    //     }else {
+    //
+    //         const json_array = result;
+    //         let csv_string = '';
+    //         const titles = Object.keys(result[0]);
+    //
+    //         titles.forEach((title, index)=>{ csv_string += (index !== titles.length-1 ? `${title},` : `${title}\r\n`); });
+    //         json_array.forEach((content, index)=> {
+    //
+    //             let row = ''; // 각 인덱스에 해당하는 '내용'을 담을 행
+    //
+    //             for (let title in content) {
+    //                 row += (row === '' ? `${content[title]}` : `,${content[title]}`);
+    //             }
+    //
+    //             csv_string += (index !== json_array.length-1 ? `${row}\r\n`: `${row}`);
+    //
+    //         })
+    //
+    //         fs.writeFileSync('1.csv', csv_string);
+    //
+    //         res.setHeader("Content-disposition", "attachment; filename="+ date.getTime() +'.csv');
+    //         res.set('Content-Type', 'text/csv');
+    //         fs.createReadStream('./1.csv')
+    //             .pipe(res)
+    //             .on('finish', ()=>{
+    //             console.log('download complete')
+    //         })
+    //     }
+    // })
+
+
+    res.send('hello')
+
+
 })
 
 module.exports = router;
